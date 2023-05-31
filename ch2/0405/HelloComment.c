5924ae009c50a424b109e773e8857716f04e80ed
yjjyjjyjj <131969401+yjjyjjyjj@users.noreply.github.com>
2023-05-31 14:30:00 +0900

    Add files via upload

---
+++ ch2/0405/HelloComment.c
@@ -0,0 +1,17 @@
+/*
+제목: Hello world 츨력하기
+기능: 문자열 출력
+파일이름: HelloComment.c
+수정날짜: 2023.04.06
+작성자:양정주
+*/
+#include <stdio.h>
+int main(void)
+{
+	/*
+	이 함수 내에서는 하나의 문자열을 출력한다.
+	문자열은 모니터로 출력한다.
+	*/
+	printf("Hello world! \n");  //문자열의 출력
+	return 0;
+}
---
+++ ch2/0405/ex02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("이름: 양정주 \n");
+	printf("주소: 광주광역시 \n");
+	printf("전화번호: 010-4430- \n");
+	return 0;
+}
+
