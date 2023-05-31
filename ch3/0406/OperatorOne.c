ddaec4409881a494ed810753f30a98162735afaa
yjjyjjyjj <131969401+yjjyjjyjj@users.noreply.github.com>
2023-05-31 14:30:34 +0900

    Add files via upload

---
+++ ch3/0406/OperatorOne.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1=9, num2=2;
+	printf("%d+%d=%d \n", num1, num2, num1+num2);
+	printf("%d-%d=%d \n", num1, num2, num1-num2);
+	printf("%dX%d=%d \n", num1, num2, num1*num2);
+	printf("%d÷%d=%d \n", num1, num2, num1/num2);
+	printf("%d÷%d=%d \n", num1, num2, num1%num2);
+	return 0;
+}
