7ae1584e75d1bad379664a50eda36ba53c5c5af2
yjjyjjyjj <131969401+yjjyjjyjj@users.noreply.github.com>
2023-05-31 14:47:59 +0900

    Add files via upload

---
+++ ch21/NeedlnputBufFlush.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char perID[7];
+    char name[10];
+
+    fputs("주민번호 앞 6자리 입력: ", stdout);
+    fgets(perID, sizeof(perID), stdin);
+
+    fputs("이름 입력: ", stdout);
+
+    printf("주민번호: %s \n", perID);
+    printf("이름: %s \n", name);
+    return 0;
+}
---
+++ ch21/Re.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ch1, ch2;
+
+    ch1=getchar();
+    ch2=fgetc(stdin);
+
+    putchar(ch1);
+    fputc(ch2, stdout);
+    return 0;
+}
